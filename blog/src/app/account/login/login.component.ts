d71a468daca4828a2c0c9b08888bc68867b766c0
Domel <domel1162@gmail..com>
2021-04-30 15:56:30 +0200

    prod version generated

---
+++ blog/src/app/account/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ogin',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
